82837f18c118261b4faa12a9a29305e5536074a1
kyle-stamm <stamm.kyle@ufl.edu>
2024-10-19 11:41:06 -0400

    wait time data

---
+++ cmake-build-debug/CMakeFiles/ImagineersFinalProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImagineersFinalProject.dir/main.cpp.obj"
+  "CMakeFiles/ImagineersFinalProject.dir/main.cpp.obj.d"
+  "ImagineersFinalProject.exe"
+  "ImagineersFinalProject.exe.manifest"
+  "ImagineersFinalProject.pdb"
+  "libImagineersFinalProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImagineersFinalProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ImagineersFinalProject.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ImagineersFinalProject.
+# This may be replaced when dependencies are built.
